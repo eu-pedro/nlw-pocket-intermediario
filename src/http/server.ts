94d203c18236c6d85787d150d48612dc750b65e5
pedro henrique <pedro.henrike2812@gmail.com>
2024-09-15 18:22:30 -0300

    feat: aula 01

---
+++ src/http/server.ts
@@ -0,0 +1,65 @@
+import fastify from 'fastify'
+import { createGoal } from '../functions/create-goal'
+import {
+  serializerCompiler,
+  validatorCompiler,
+  ZodTypeProvider,
+} from 'fastify-type-provider-zod'
+import z from 'zod'
+import { getWeekPendingGoals } from '../functions/get-week-pending-goals'
+import { createGoalCompletion } from '../functions/create-goal-completion'
+
+const app = fastify().withTypeProvider<ZodTypeProvider>()
+
+app.setValidatorCompiler(validatorCompiler)
+app.setSerializerCompiler(serializerCompiler)
+
+app.post(
+  '/goals',
+  {
+    schema: {
+      body: z.object({
+        title: z.string(),
+        desiredWeeklyFrequency: z.number().min(1).max(7),
+      }),
+    },
+  },
+  async (request) => {
+    const { desiredWeeklyFrequency, title } = request.body
+
+    await createGoal({
+      title,
+      desiredWeeklyFrequency,
+    })
+  },
+)
+
+app.post(
+  '/completion',
+  {
+    schema: {
+      body: z.object({
+        goalId: z.string(),
+      }),
+    },
+  },
+  async (request) => {
+    const { goalId } = request.body
+
+    await createGoalCompletion({
+      goalId,
+    })
+  },
+)
+
+app.get('/pending-goals', async () => {
+  const { pendingGoals } = await getWeekPendingGoals()
+
+  return { pendingGoals }
+})
+
+app
+  .listen({
+    port: 3333,
+  })
+  .then(() => console.log('http server running!'))
